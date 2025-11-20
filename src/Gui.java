54171e0aa6c9fb1995c7f189bf4513aa1c72242c
R4ZZ-ETH <simone.napolitano08@gmail.com>
2025-11-20 20:41:26 +0100

    Added a GUI

--- src/Gui.java
+++ src/Gui.java
@@ -1,10 +1,36 @@
 import javax.swing.*;
+import java.awt.*;;
 
 public class Gui extends JFrame {
+    Cypher cypher = new Cypher("3591255939063529");
+    JLabel resultLabel;
+    String result;
+    JTextField input;
+    JButton cipherButton, decipherButton;
     public Gui(){
+        resultLabel = new JLabel("Result: ");
+        input = new JTextField(20);
+        cipherButton = new JButton("Cipher");
+        decipherButton = new JButton("Decipher");
         setTitle("Cypher-Decypher");
-        setSize(500, 500);
+        setSize(2000, 100);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        JPanel panel = new JPanel(new GridLayout(1, 4, 4, 4));
+        panel.add(input);
+        panel.add(cipherButton);
+        cipherButton.addActionListener(e->{
+            result = cypher.encrypt(input.getText());
+            resultLabel.setText("Result: " + result);
+        });
+        panel.add(decipherButton);
+        decipherButton.addActionListener(e->{
+            result = cypher.decrypt(input.getText());
+            resultLabel.setText("Result: " + result);
+        });
+        panel.add(resultLabel);
+        add(panel);
         setVisible(true);
     }   
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
 import java.util.Scanner;
-//?Jjq, ricordare questo per poi provare a decriptarlo
 public class Main {
-    Scanner input = new Scanner(System.in);
-    public Main(){
-        System.out.println("Input the message:");
-        String message = input.nextLine();
-        System.out.println("Encrypt or decrypt?");
-        Cypher cypher = new Cypher("3591255939063529");
-        int scelta = input.nextInt();
-        switch(scelta){
-            case 1:               
-                System.out.println("The encrypted message is: " + cypher.encrypt(message));
-                break;
-            case 2:
-                System.out.println("The decrypted message is: " + cypher.decrypt(message));
-                break;
-        }
-    }
     public static void main(String[] args) {
-        new Main();
+        new Gui();
     }
 }
